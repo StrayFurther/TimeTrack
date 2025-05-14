76f5ac6120e4ef8b2d52a92c02d9d3e0ca04b4d6
StrayFurther <t.waldraff95@gmx.de>
2025-05-14 12:57:48 +0200

    correct ui

--- frontend/src/app/pages/projects/edit-project-modal/edit-project-modal.component.ts
+++ frontend/src/app/pages/projects/edit-project-modal/edit-project-modal.component.ts
@@ -1,12 +1,19 @@
 import { Component, inject, OnInit } from '@angular/core';
 import {Project} from '../../../models/project';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent,
+  MatDialogModule, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
 import { MatLabel } from '@angular/material/form-field';
+import {MatInput, MatInputModule} from '@angular/material/input';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import {MatButton} from '@angular/material/button';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'edit-project-modal',
-  imports: [ReactiveFormsModule, MatDialogClose, MatDialogActions, MatDialogContent, MatDialogTitle, MatLabel],
+  imports: [ReactiveFormsModule, CommonModule, MatDialogActions, MatDialogContent, MatDialogTitle, MatLabel, MatInput, CdkTextareaAutosize, MatButton, ReactiveFormsModule,
+    MatInputModule,
+    MatDialogModule],
   templateUrl: './edit-project-modal.component.html',
   styleUrl: './edit-project-modal.component.scss'
 })
