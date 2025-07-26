b34b3972fb923caba3531402b3bb9c0ea65ab59d
StrayFurther <t.waldraff95@gmx.de>
2025-07-26 12:46:31 +0200

    adjusted

--- backend/src/test/java/strayfurther/backend/util/DirectoryPermissionManagerTest.java
+++ backend/src/test/java/strayfurther/backend/util/DirectoryPermissionManagerTest.java
@@ -3,6 +3,8 @@ package strayfurther.backend.util;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,6 +17,8 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
+@SpringBootTest
 class DirectoryPermissionManagerTest {
 
     private Path tempDirectory;
